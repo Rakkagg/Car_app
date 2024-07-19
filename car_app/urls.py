c3ebf9a29401f7bf186e3dbe05f65dafd8fc158f
Charles Dacanay <charles.dacanay@valens-research.com>
2024-07-19 16:06:15 +0800

    Created a query to move the drag and drop the cars

--- car_app/urls.py
+++ car_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.display_cars, name='car_list'),
+    path('switch_car/', views.switch_car, name='switch_car'),
 ]
--- car_app/views.py
+++ car_app/views.py
@@ -10,3 +10,17 @@ def display_cars(request):
     else:
         cars = Car.objects.all().order_by('carPosition')
     return render(request, 'car_list.html', {"cars": cars})
+
+
+def switch_car(request):
+    if request.method == 'POST':
+        new_positions = request.POST.getlist('car[]')
+        for index, car_id in enumerate(new_positions):
+            car = Car.objects.get(id=car_id)
+            car.carPosition = index
+            car.save()
+        return JsonResponse({'message': 'Car positions updated successfully'})
+    return JsonResponse({'error': 'Invalid request'}, status=400)
+
+
+
