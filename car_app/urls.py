c0e66ebde911d7b3923b7d9c53ed5563a1da8797
Charles Dacanay <charles.dacanay@valens-research.com>
2024-07-19 14:59:26 +0800

    updated query

--- car_app/urls.py
+++ car_app/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.display_cars, name='display_cars'),
-    path('sorting_red_cars/', views.sorting_red_cars, name='sorting_red_cars'),
-    path('sorting_blue_cars/', views.sorting_blue_cars, name='sorting_blue_cars'),
-    path('post_switch_car/', views.post_switch_car, name='post_switch_car'),
+    path('', views.display_cars, name='car_list'),
 ]
--- car_app/views.py
+++ car_app/views.py
@@ -1,27 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from .models import Car
 
+
 def display_cars(request):
-    cars = Car.objects.all().order_by('carPosition')
+    color = request.GET.get('color')
+    if color:
+        cars = Car.objects.filter(carColor__iexact=color)
+    else:
+        cars = Car.objects.all().order_by('carPosition')
     return render(request, 'car_list.html', {"cars": cars})
-
-def sorting_red_cars(request):
-    red_cars = Car.objects.filter(carColor__iexact='red').order_by('carPosition')
-    return render(request, 'car_list.html', {'cars': red_cars})
-
-def sorting_blue_cars(request):
-    blue_cars = Car.objects.filter(carColor__iexact='blue').order_by('carPosition')
-    return render(request, 'car_list.html', {'cars': blue_cars})
-
-@csrf_exempt
-def post_switch_car(request):
-    if request.method == 'POST':
-        new_positions = request.POST.getlist('car[]')
-        for index, car_id in enumerate(new_positions):
-            car = Car.objects.get(id=car_id)
-            car.carPosition = index
-            car.save()
-        return JsonResponse({'message': 'Car positions updated successfully'})
-    return JsonResponse({'error': 'Invalid request'}, status=400)
