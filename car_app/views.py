0c1cfa36203f19dc338d6b70a4acaa6932d883e1
Charles Dacanay <charles.dacanay@valens-research.com>
2024-07-22 14:03:43 +0800

    added a function that update the position of the cars to database

--- car_app/views.py
+++ car_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+
 from .models import Car
 
 
@@ -15,12 +16,7 @@ def display_cars(request):
 def switch_car(request):
     if request.method == 'POST':
         new_positions = request.POST.getlist('car[]')
-        for index, car_id in enumerate(new_positions):
-            car = Car.objects.get(id=car_id)
-            car.carPosition = index
-            car.save()
+        cars_to_update = [Car(id=car_id, carPosition=index) for index, car_id in enumerate(new_positions)]
+        Car.objects.bulk_update(cars_to_update, ['carPosition'])
         return JsonResponse({'message': 'Car positions updated successfully'})
     return JsonResponse({'error': 'Invalid request'}, status=400)
-
-
-
